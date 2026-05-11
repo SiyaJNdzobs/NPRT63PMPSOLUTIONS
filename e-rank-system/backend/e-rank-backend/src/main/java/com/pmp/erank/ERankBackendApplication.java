7f09cde77add79272f8b301dccec55304889ca07
SiyaJNdzobs <siyajndzobs@gmail.com>
2026-05-11 21:55:48 +0200

    Spring Boot backend setup complete - BUILD SUCCESS

---
+++ e-rank-system/backend/e-rank-backend/src/main/java/com/pmp/erank/ERankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.pmp.erank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERankBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ERankBackendApplication.class, args);
+    }
+
+}
